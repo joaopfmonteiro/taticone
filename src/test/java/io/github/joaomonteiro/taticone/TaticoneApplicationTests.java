c72670373bdb6f71d9b7ad147b948993edfa6625
joaopfmonteiro <joaoosverdes@gmail.com>
2025-07-11 07:23:48 +0200

    Initial commit: Spring boot project setup

---
+++ src/test/java/io/github/joaomonteiro/taticone/TaticoneApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.joaomonteiro.taticone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaticoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
