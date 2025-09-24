55bec5853b43de41cd41715630a648ab3031750b
João Monteiro <joaopfmonteiro3@gmail.com>
2025-09-24 13:04:16 +0200

    test and auth controller refactor

--- src/main/java/io/github/joaomonteiro/taticone/config/SecurityConfig.java
+++ src/main/java/io/github/joaomonteiro/taticone/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package io.github.joaomonteiro.taticone.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
